85b0dcbe0c3061d5ee066d5d92950a1e22cf25b1
36chubm54 <102345086+36chubm54@users.noreply.github.com>
2025-06-10 21:54:09 +0500

    Add script to install and apply powerlevel10k theme with plugins

--- apply_zsh_theme.sh
+++ apply_zsh_theme.sh
@@ -8,8 +8,9 @@ cat ~/.zshrc | sed 's|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-
 mv ~/.zshrc_new ~/.zshrc
 
 # Install autocomplete and syntax highlighting plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
 
 # Restarting the terminal
 exec zsh
