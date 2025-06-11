e14e7546be8a13d33624c70b3dee1e3e8612e308
36chubm54 <102345086+36chubm54@users.noreply.github.com>
2025-06-11 19:57:54 +0500

    Add script to install and apply powerlevel10k theme with plugins

--- apply_zsh_theme.sh
+++ apply_zsh_theme.sh
@@ -3,14 +3,13 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-
 
 # Replacing strings in the config
 cat ~/.zshrc | sed 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|' > ~/.zshrc_new
-mv ~/.zshrc_new ~/.zshrc
-cat ~/.zshrc | sed 's|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)|' > ~/.zshrc_new
+cat ~/.zshrc | sed 's|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-autocomplete)|' > ~/.zshrc_new
 mv ~/.zshrc_new ~/.zshrc
 
 # Install autocomplete and syntax highlighting plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
 
 # Restarting the terminal
 exec zsh
